d920a8b88469af029552f548f5f1cbc4ed0ebc3a
victordsilarmoreira <victordsilar_@hotmail.com>
2025-02-15 12:05:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const registeredLogin = "Pratique";
 const registeredPassword = "Estacionamento1402";
 
+function showMessage(message) {
+    const messageBox = document.getElementById("messageBox");
+    messageBox.textContent = message;
+    messageBox.style.display = "block";
+    messageBox.style.opacity = "1";
+
+    setTimeout(() => {
+        messageBox.style.opacity = "0";
+        setTimeout(() => {
+            messageBox.style.display = "none";
+        }, 500);
+    }, 3000);
+}
+
 function submitLogin() {
     var login = document.getElementById("login").value;
     var senha = document.getElementById("senha").value;
 
     if (login === registeredLogin && senha === registeredPassword) {
-        alert("Login bem-sucedido!");
+        showMessage("Login bem-sucedido!");
         document.getElementById("loginForm").style.display = "none";
         document.getElementById("infoForm").style.display = "block";
     } else {
-        alert("Login ou senha incorretos.");
+        showMessage("Login ou senha incorretos.");
     }
 }
 
@@ -18,7 +32,7 @@ function submitInfo() {
     var nome = document.getElementById("nome").value;
     var email = document.getElementById("email").value;
     var telefone = document.getElementById("telefone").value;
-    alert("Nome: " + nome + "\nE-mail: " + email + "\nTelefone: " + telefone);
+    showMessage("Nome: " + nome + "\nE-mail: " + email + "\nTelefone: " + telefone);
 
     var HTTP = {
         "url": "https://somarcasctg.digisac.app/api/v1/messages",
@@ -28,7 +42,6 @@ function submitInfo() {
             "Content-Type": "application/json"
         },
         "body": JSON.stringify({
-             
             "type": "chat",
             "number": telefone,
             "serviceId": "167e93b2-23ce-4615-836a-fc019ad79549",
@@ -37,12 +50,11 @@ function submitInfo() {
             "files": [],
             "uploadingFiles": false,
             "replyTo": null,
-            "components": {
+            "components": [{
                 "type": "body",
                 "parameters": []
-            }
-            }
-        )
+            }]
+        })
     };
 
     fetch(HTTP.url, {
@@ -53,13 +65,12 @@ function submitInfo() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            alert("Requisição enviada com sucesso!");
+            showMessage("Requisição enviada com sucesso!");
+            // Limpar o formulário após o envio
+            document.getElementById("infoForm").reset();
         })
         .catch(error => {
             console.error('Erro:', error);
-            alert("Erro ao enviar a requisição.");
+            showMessage("Erro ao enviar a requisição.");
         });
-
-    var quantidadeLinhas = Object.keys(HTTP).length;
-    alert("Quantidade de linhas: " + quantidadeLinhas);
 }
