afe9517b77ec76c04adaee056765f46e635e41f4
victordsilarmoreira <victordsilar_@hotmail.com>
2025-02-16 17:26:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const registeredLogin = "Pratique";
-const registeredPassword = "Estacionamento1402";
+const registeredLogin = "A";
+const registeredPassword = "A";
 
 function showMessage(message) {
     const messageBox = document.getElementById("messageBox");
@@ -24,7 +24,7 @@ function submitLogin() {
         document.getElementById("loginForm").style.display = "none";
         document.getElementById("infoForm").style.display = "block";
     } else {
-        showMessage("Login ou senha incorreto");
+        showMessage("Login ou senha incorreto.");
     }
 }
 
@@ -35,58 +35,122 @@ function obterDataAtualFormatada() {
     var ano = data.getFullYear();
     return dia + '/' + mes + '/' + ano;
 }
+
 var dataAtual = obterDataAtualFormatada();
 
-function submitInfo() {
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
-    var telefone = document.getElementById("telefone").value;
-    showMessage("Nome: " + nome + "\nE-mail: " + email + "\nTelefone: " + telefone);
+function mascararTelefone(input) {
+    var telefone = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+    telefone = telefone.replace(/^(\d{2})(\d)/g, '($1) $2'); // Adiciona parênteses e espaço
+    telefone = telefone.replace(/(\d{5})(\d)/, '$1-$2'); // Adiciona hífen
+    input.value = telefone;
+}
 
-    // Função para verificar e cadastrar ou atualizar contato
-    function verificarECadastrarOuAtualizarContato(nome, telefone, email) {
-        var dataAtual = obterDataAtualFormatada();
+function formatarTelefone(telefone) {
+    if (telefone.length !== 15) {
+        showMessage("Telefone inválido");
+        return;
+    }
+  
+    var telefoneNumeros = '55' + telefone.replace(/[()\s-]/g, '');
 
-        var verificar = {
-            "url": `https://somarcasctg.digisac.app/api/v1/contacts?where[data][number]=${telefone}`,
-            "method": "GET",
-            "headers": {
-                "Authorization": "Bearer 5f549bfdd71ddbe1eab5640ce6c897764284cdaa",
-                "Content-Type": "application/json"
-            }
-        };
+    console.log("Número de telefone sem formatação:", telefoneNumeros);
 
-        // Função para lidar com a resposta da verificação
-        function handleResponse(response) {
-            if (!response || response.length === 0) {
-                showMessage("Contato não cadastrado. Cadastrando...");
-                var cadastrar = {
-                    "url": "https://somarcasctg.digisac.app/api/v1/contacts",
-                    "method": "POST",
-                    "headers": {
-                        "Authorization": "Bearer 5f549bfdd71ddbe1eab5640ce6c897764284cdaa",
-                        "Content-Type": "application/json"
-                    },
-                    "body": JSON.stringify({
-                        "name": nome,
-                        "number": telefone,
-                        "email": email,
-                        "note": dataAtual
-                    })
-                };
+    return telefoneNumeros;
+}
 
-                // Execute a requisição POST
-                fetch(cadastrar.url, {
-                    method: cadastrar.method,
-                    headers: cadastrar.headers,
-                    body: cadastrar.body
+function validarNome(nome) {
+    var regex = /^[a-zA-Z\s]+$/;
+    return regex.test(nome);
+}
+
+function validarEmail(email) {
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function verificarECadastrarOuAtualizarContato(nome, telefone, email) {
+    if (!validarEmail(email)) {
+        showMessage("E-mail inválido.");
+        return;
+    }
+    if (!validarNome(nome)) {
+        showMessage("Nome inválido");
+        return;
+    }
+    var phone =  telefone.replace(/[()\s-]/g, '');
+    console.log("Telefone para cadastro:", phone)
+    if (phone === null) {
+        return;
+    };
+    var verificar = {
+        "url": `https://somarcasctg.digisac.app/api/v1/contacts?where[data][number]=${phone}`,
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer 5f549bfdd71ddbe1eab5640ce6c897764284cdaa",
+            "Content-Type": "application/json"
+        }
+    };
+
+    console.log("URL da verificação:", verificar.url); // Log da URL da verificação
+
+    // Função para lidar com a resposta da verificação
+    function handleResponse(response) {
+        console.log("Resposta da verificação:", response); // Log da resposta da verificação completa
+        if (!response.data || response.data.length === 0) {
+            showMessage("Contato não cadastrado. Cadastrando...");
+            var cadastrar = {
+                "url": "https://somarcasctg.digisac.app/api/v1/contacts",
+                "method": "POST",
+                "headers": {
+                    "Authorization": "Bearer 5f549bfdd71ddbe1eab5640ce6c897764284cdaa",
+                    "Content-Type": "application/json"
+                },
+                "body": JSON.stringify({
+                    "internalName": nome,
+                    "number": phone,
+                    "serviceId": "167e93b2-23ce-4615-836a-fc019ad79549",
+                    "note": dataAtual,
+                    "defaultDepartmentId": "9d76e703-bf88-4c17-bfeb-0f2de8f99e80",
+                    "tagIds": [
+                        "6790bd44-53b5-4253-878d-64c0ca509d26"
+                    ],
+                    "custom-fields": [
+                        {
+                            "id": "b9a3d696-cc15-4ab3-a880-445705dfc263",
+                            "value": email
+                        }
+                    ]
                 })
-                    .then(response => response.json())
-                    .then(data => showMessage('Contato criado:', data))
-                    .catch(error => console.error('Erro:', error));
-            } else {
-                showMessage("Contato já cadastrado.");
-                var contatoId = response[0].id;
+            };
+
+            console.log("Dados do cadastro:", cadastrar.body); // Log dos dados do cadastro
+
+            // Execute a requisição POST
+            fetch(cadastrar.url, {
+                method: cadastrar.method,
+                headers: cadastrar.headers,
+                body: cadastrar.body
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        return response.text().then(text => { throw new Error(text) });
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    showMessage('Contato criado:', data);
+                    enviarMensagem(data.id, phone, nome);
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                    showMessage('Erro ao criar contato: ' + error.message);
+                });
+        } else {
+            showMessage("Contato já cadastrado.");
+            console.log("Primeiro item da resposta:", response.data[0]); // Log do primeiro item da resposta
+            if (response.data[0] && response.data[0].id) {
+                var contatoId = response.data[0].id;
+                console.log("ID do contato:", contatoId); // Log do ID do contato
                 var editarcontact = {
                     "url": `https://somarcasctg.digisac.app/api/v1/contacts/${contatoId}`,
                     "method": "PUT",
@@ -106,20 +170,36 @@ function submitInfo() {
                     body: editarcontact.body
                 })
                     .then(response => response.json())
-                    .then(data => showMessage('Contato atualizado:', data))
+                    .then(data => {
+                        console.log("Resposta da atualização:", data); // Log da resposta da atualização
+                        showMessage('Contato atualizado:', data);
+                        enviarMensagem(contatoId, phone, nome);
+                    })
                     .catch(error => console.error('Erro:', error));
+            } else {
+                console.error('Erro: ID do contato não encontrado na resposta.');
+                showMessage('Erro ao obter o ID do contato.');
             }
         }
-
-        // Execute a requisição GET
-        fetch(verificar.url, {
-            method: verificar.method,
-            headers: verificar.headers
-        })
-            .then(response => response.json())
-            .then(data => handleResponse(data))
-            .catch(error => console.error('Erro:', error));
     }
+
+    // Execute a requisição GET
+    fetch(verificar.url, {
+        method: verificar.method,
+        headers: verificar.headers
+    })
+        .then(response => response.json())
+        .then(data => handleResponse(data))
+        .catch(error => console.error('Erro:', error));
+}
+
+function enviarMensagem(contatoId, telefone, nome) {
+    var phone1 = telefone.replace(/[()\s-]/g, '');
+    console.log("Telefone para cadastro:", phone1);
+    if (phone1 === null) {
+        return;
+    }
+    var telefoneFormatado1 = formatarTelefone(telefone);
     var HTTP = {
         "url": "https://somarcasctg.digisac.app/api/v1/messages",
         "method": "POST",
@@ -129,10 +209,10 @@ function submitInfo() {
         },
         "body": JSON.stringify({
             "type": "chat",
-            "contactId": contatoId,
+            "number": phone1,
             "serviceId": "167e93b2-23ce-4615-836a-fc019ad79549",
             "dontOpenTicket": "true",
-            "hsmId": "15e6ad1e-d019-4474-9749-d6b280ead614",
+            "hsmId": "f45b2e5e-8fa0-4e10-a21f-d893b167de91",
             "files": [],
             "uploadingFiles": false,
             "replyTo": null,
@@ -159,6 +239,7 @@ function submitInfo() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            console.log(data.number); // Corrigido aqui
             showMessage("Requisição enviada com sucesso!");
             // Limpar o formulário após o envio
             document.getElementById("infoForm").reset();
@@ -167,4 +248,17 @@ function submitInfo() {
             console.error('Erro:', error);
             showMessage("Erro ao enviar a requisição.");
         });
+}
+
+function submitInfo() {
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var telefone = document.getElementById("telefone").value;
+
+    var telefoneFormatado = formatarTelefone(telefone);
+    if (!telefoneFormatado) {
+        return; // Se o telefone não for válido, interrompe a execução
+    }
+
+    verificarECadastrarOuAtualizarContato(nome, telefoneFormatado, email);
 }
